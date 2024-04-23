790fe07fd7c2f4fdf3bea682ad50077ef3c072a4
zhouziwang.joyboy <zhouziwang.joyboy@bytedance.com>
2024-04-23 10:57:38 +0800

    新增 通过 InitializingBean 实现初始化配置

--- spring-cloud-gateway/src/main/java/com/gateway/config/WhiteIPConfig.java
+++ spring-cloud-gateway/src/main/java/com/gateway/config/WhiteIPConfig.java
@@ -3,6 +3,7 @@ package com.gateway.config;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.HashMap;
@@ -12,8 +13,12 @@ import java.util.Map;
 @Configuration
 @Getter
 @Setter
-public class WhiteIPConfig {
+public class WhiteIPConfig implements InitializingBean {
     private Map<String, List<String>> innerMap = new HashMap<>();
 
 
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // TODO: 通过feign接口调用
+    }
 }
