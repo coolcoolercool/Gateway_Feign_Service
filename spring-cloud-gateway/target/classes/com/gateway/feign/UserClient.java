b1d9c9d9df2d455f7a06e42f495f77e97c4b9263
zhouziwang.joyboy <zhouziwang.joyboy@bytedance.com>
2024-06-05 14:03:50 +0800

    在SpringBoot3.x JDK17的环境中使用reactive feign调用其他服务

---
+++ spring-cloud-gateway/target/classes/com/gateway/feign/UserClient.java
@@ -0,0 +1,20 @@
+package com.gateway.feign;
+
+
+/*import com.gateway.response.ResultResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import reactor.core.publisher.Mono;
+
+@SuppressWarnings("rawtypes")
+@FeignClient(name = "user-feign")
+public interface UserClient {
+
+    // 这里的Cookie通过header请求头设置，透传给用户鉴权模块
+    @GetMapping(value = "/feign/api/v1/users/checkPermission", headers = "Cookie={cookie}")
+    ResultResponse checkPermission(@RequestParam String url, @RequestParam String httpMethod, @RequestParam String cookie);
+
+    @GetMapping(value = "/feign/api/v1/users/getUserInfo")
+    ResultResponse getFeignUserInfo();
+}*/
